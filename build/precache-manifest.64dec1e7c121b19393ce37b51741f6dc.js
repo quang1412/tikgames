e39d44880a73a35a9ca6fd9a3514f9580289ef64
Glitch (angelostavrow-starter-cra-and-react) <none>
2022-10-29 20:00:49 +0700

    build

---
+++ build/precache-manifest.64dec1e7c121b19393ce37b51741f6dc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bac76b89e7571f7eed27897732a3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c9b522c67a91553ad4",
+    "url": "/static/css/4.61e4c316.chunk.css"
+  },
+  {
+    "revision": "1f72bc26537dd7f72a1a",
+    "url": "/static/css/5.24ae40f5.chunk.css"
+  },
+  {
+    "revision": "3b7fb15a340c3c0bcd1f",
+    "url": "/static/css/6.467588bd.chunk.css"
+  },
+  {
+    "revision": "0299fa753352160bd251",
+    "url": "/static/css/7.a489fc13.chunk.css"
+  },
+  {
+    "revision": "51ccd81dee5d0bf724bb",
+    "url": "/static/css/8.5d53711e.chunk.css"
+  },
+  {
+    "revision": "ebed7bfd69d619cb57e3",
+    "url": "/static/css/main.4538322a.chunk.css"
+  },
+  {
+    "revision": "ff2c9b792ae0f2038976",
+    "url": "/static/js/0.14acd0ea.chunk.js"
+  },
+  {
+    "revision": "c307d458d54c9e1524984b0ae1f3e612",
+    "url": "/static/js/0.14acd0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9702065297055be01e0",
+    "url": "/static/js/1.84e1d559.chunk.js"
+  },
+  {
+    "revision": "08c9b522c67a91553ad4",
+    "url": "/static/js/4.3f330774.chunk.js"
+  },
+  {
+    "revision": "78d8353da635afbf52949a3d44b951a3",
+    "url": "/static/js/4.3f330774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f72bc26537dd7f72a1a",
+    "url": "/static/js/5.720eafff.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/5.720eafff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7fb15a340c3c0bcd1f",
+    "url": "/static/js/6.c599ad42.chunk.js"
+  },
+  {
+    "revision": "0299fa753352160bd251",
+    "url": "/static/js/7.632fb656.chunk.js"
+  },
+  {
+    "revision": "51ccd81dee5d0bf724bb",
+    "url": "/static/js/8.02d7c80d.chunk.js"
+  },
+  {
+    "revision": "ebed7bfd69d619cb57e3",
+    "url": "/static/js/main.742c1077.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.742c1077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6c7e0ae46964c10cd2",
+    "url": "/static/js/runtime-main.45bddfe2.js"
+  }
+]);
